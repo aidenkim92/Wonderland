8307a22ccb14066a496c9a7f2abbcc34be8d1f95
aidenkim92 <dlqnrla@gmail.com>
2020-05-07 09:37:10 +1200

    AI Turret added
    1. The Turret detect bidirection of Player's preferences then it twards to the Player and start to attack using Bullets.
    2. The Turret stays under the ground untill the Player approach certain boundaries.
    3. When the Player health bar is created then it should give the damages to the Player and Destroy the bullet once it conflicts to the Player(code is ready)
    4. Added Turret(did not used Asset Store), and Bullets have both animators and have put into the Prefab.

---
+++ Assets/Scripts/Monsters/TurretMonsterAttackCone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMonsterAttackCone : MonoBehaviour
+{
+    public TurretMonster turretMonster;
+
+    public bool isLeft = false;
+
+    private void Awake()
+    {
+        turretMonster = gameObject.GetComponentInParent<TurretMonster>();    
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            if(isLeft)
+            {
+                turretMonster.Attack(false);
+            } 
+            else if(!isLeft)
+            {
+                turretMonster.Attack(true);
+            }
+        }
+    }
+}
