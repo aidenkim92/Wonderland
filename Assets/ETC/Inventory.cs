7d6168c816d33c230dca4ab0ab55a53e2a64a7c8
aidenkim92 <dlqnrla@gmail.com>
2020-05-10 07:05:04 +1200

    Modified Functionality and UI for Inventory
    1. Modified ESC function key to pause the game, and it allows the player to select their preferences(e.g Restart,Resume,Quit,Settings,Main Menu)
    2. Modified Inventory UI that allows the Player to press the key to open and close the Inventory (e.g press 'i'), And default slots for the number of items can handle is 4, however, it also modified that now add slots to click the '+' button on the Inventory.

---
+++ Assets/ETC/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+    #endregion//Singleton Pattern
+
+
+    //Using Delegate that informs the user to there might be created items.
+    public delegate void OnSlotCountChange(int val); //Definition
+    public OnSlotCountChange onSlotCountChange;//Instantiate the Definition
+
+    private int slotCnt;
+    public int SlotCnt
+    {
+        get => slotCnt;
+        set
+        {
+            slotCnt = value;
+            onSlotCountChange.Invoke(slotCnt);
+        }
+    }
+
+    void Start()
+    {
+        SlotCnt = 4;
+    }
+}
--- Assets/ETC/InventoryUI.cs
+++ Assets/ETC/InventoryUI.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    Inventory inven;
     public GameObject inventoryPanel;
     bool activeInventory = false;
 
+    public Slot[] slots;
+    public Transform slotHolder;
     void Start()
     {
+        inven = Inventory.instance;
+        slots = slotHolder.GetComponentsInChildren<Slot>();
+        inven.onSlotCountChange += SlotChange;
         inventoryPanel.SetActive(activeInventory);
     }
+
+    //to interact with the slots
+    private void SlotChange(int val)
+    {
+        for(int i = 0; i<slots.Length; i++)
+        {
+            if(i < inven.SlotCnt)
+            {
+                slots[i].GetComponent<Button>().interactable = true;
+            }
+            else
+            {
+                slots[i].GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.I))
@@ -18,5 +43,11 @@ public class InventoryUI : MonoBehaviour
             activeInventory = !activeInventory;
             inventoryPanel.SetActive(activeInventory);
         }
+    }
+    
+    //Add the slots for the inventory
+    public void AddSlot()
+    {
+        inven.SlotCnt++;
     }
 }
