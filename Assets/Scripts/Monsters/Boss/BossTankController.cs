7625f3ca39b370330f83f705b352d18bb6fc89ec
shifaaz <shkhan078@gmail.com>
2020-05-21 12:51:57 +1200

    Added basic Boss scene and boss scenario
    Spawns bullets
    Can hit boss and defeat it
    Boss moves around witha predetermined pattern

---
+++ Assets/Scripts/Monsters/Boss/BossTankController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTankController : MonoBehaviour
+{
+    public enum bossStates { shooting, hurt, moving };
+    public bossStates currentState;
+
+    public Transform theBoss;
+    public Animator anim;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public Transform leftPoint, rightPoint;
+    private bool moveRight;
+
+    [Header("Shooting")]
+    public GameObject bullet;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    [Header("Hurt")]
+    public float hurtTime;
+    public float hurtCounter;
+
+    [Header("Health System")]
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = bossStates.shooting;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case bossStates.shooting:
+               
+                shotCounter -= Time.deltaTime;
+
+                if(shotCounter <= 0)
+                {
+                    shotCounter = timeBetweenShots;
+
+                  var newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    newBullet.transform.localScale = theBoss.localScale;
+                }
+
+                break;
+
+            case bossStates.hurt:
+                if(hurtCounter > 0)
+                {
+                    hurtCounter -= Time.deltaTime;
+
+                    if(hurtCounter <= 0)
+                    {
+                        currentState = bossStates.moving;
+                    }
+                }
+
+                break;
+
+            case bossStates.moving:
+
+                if(moveRight)
+                {
+                    theBoss.position += new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+
+                    if (theBoss.position.x > rightPoint.position.x)
+                    {
+
+                        theBoss.localScale = new Vector3(1f, 1f, 1f);
+                        moveRight = false;
+
+                        EndMovement();
+                    }
+                }
+
+                else
+                {
+                    theBoss.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+
+                    if (theBoss.position.x < leftPoint.position.x)
+                    {
+                        theBoss.localScale = new Vector3(-1f, 1f, 1f);
+                        moveRight = true;
+                        EndMovement();
+                    }
+                }
+
+                break;
+        }
+
+#if UNITY_EDITOR
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            Damage(1);
+        }
+
+#endif
+
+    }
+
+
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        currentState = bossStates.hurt;
+        hurtCounter = hurtTime;
+
+        anim.SetTrigger("Hit");
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            AudioManager.instance.PlaySFX(8);
+        }
+            
+    }
+
+    private void EndMovement()
+    {
+        currentState = bossStates.shooting;
+        shotCounter = timeBetweenShots;
+
+        anim.SetTrigger("StopMoving");
+    }
+
+}
---
+++ Assets/Scripts/Monsters/Boss/BossTankHitbox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTankHitbox : MonoBehaviour
+{
+
+    public BossTankController bossCont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player has hit");
+        }
+    }
+
+
+}
