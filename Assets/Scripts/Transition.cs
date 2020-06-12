5598f5fca30481bb67513f8b5eede430e1e0e7a1
Naera <xzv1969@autuni.ac.nz>
2020-06-13 06:07:11 +1200

    Player prefab updated
    Prefab now contains reference to the image cooldowns
    Messed around with tranisitoning to different scenes hence the added scripts

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Animator transition;
+    // Update is called once per frame
+    void Start()
+    {
+        transition.SetTrigger("start");
+    }
+}
