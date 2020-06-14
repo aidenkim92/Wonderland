4ede0fd7908c5bf24a7063024d00b711d516ca7f
shifaaz <shkhan078@gmail.com>
2020-06-14 23:02:40 +1200

    Fixed rock guy
    Now he falls, kills you and resets

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -6,15 +6,16 @@ public class Rock : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    private Player player;
+    public Player player;
     private Vector3 originalPos;
+    private bool isFallen;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        player = FindObjectOfType<Player>();
+        //player = FindObjectOfType<Player>();
         originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 
 
@@ -23,9 +24,11 @@ public class Rock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y >= -100)
+        if(player.transform.position.y >= -100 && isFallen)
         {
             gameObject.transform.position = originalPos;
+            rb.isKinematic = true;
+            isFallen = false;
 
         }
 
@@ -38,11 +41,14 @@ public class Rock : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !isFallen)
             {
             rb.isKinematic = false;
+            isFallen = true;
 
         }
+
+
 
     }
 }
