424fcfc9ea8ebad66cf0c1c9409ad3a96d79170a
shifaaz <shkhan078@gmail.com>
2020-06-17 12:02:12 +1200

    Added Hell Ending Scene

---
+++ Assets/Scripts/CutScenes/LoadBigBoss.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class LoadBigBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public double videoLength;
+    public VideoPlayer vp;
+    
+    void Start()
+    {
+        vp = gameObject.GetComponent<VideoPlayer>();
+        vp.Play();
+        //Player.instance.currentMapName = "BigBoss";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(vp.time);
+
+        if (vp.time >= videoLength)
+        {
+
+            SceneManager.LoadScene(7);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene(7);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/CutScenes/LoadBossOpening.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class LoadBossOpening : MonoBehaviour
+{
+    public double videoLength;
+
+    public VideoPlayer vp;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = gameObject.GetComponent<VideoPlayer>();
+        vp.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(vp.time);
+        
+        if (vp.time >= videoLength)
+        {
+            
+            SceneManager.LoadScene(10);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene(10);
+
+        }
+
+    }
+}
